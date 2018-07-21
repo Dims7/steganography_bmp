35d20f04e6eafbd7a69c1c4fcd031d18f2e881d5
Dims <steelbreedman@gmail.com>
2018-07-21 10:14:12 +0500

    Добавил тесты для кодера/декодера текстов

---
+++ steganography.py
@@ -0,0 +1,25 @@
+class Steganography:
+
+    @staticmethod
+    def encode_to_bmp(file_name, message):
+        pass
+
+    @staticmethod
+    def decode_from_bmp(file_name):
+        pass
+
+    @staticmethod
+    def _encode_text(input_text):
+        return input_text
+
+    @staticmethod
+    def _decode_text(input_text):
+        return input_text
+
+    @staticmethod
+    def _get_text_hashcode(input_text):
+        pass
+
+    @staticmethod
+    def _prepare_text_to_encode(input_text):
+        pass
---
+++ tests.py
@@ -0,0 +1,37 @@
+import unittest
+from steganography import Steganography
+import random
+import string
+
+
+class TestCodeText(unittest.TestCase):
+
+    def make_test(self, text):
+        text_for_code = text
+        encoded_text = Steganography._encode_text(text_for_code)
+        decoded_text = Steganography._decode_text(encoded_text)
+        self.assertEqual(text_for_code, decoded_text)
+
+    def test_empty_string(self):
+        self.make_test("")
+
+    def test_simple_strings(self):
+        self.make_test("a")
+        self.make_test("abc")
+        self.make_test("lol")
+        self.make_test("Hello")
+
+    def test_symbols(self):
+        self.make_test(".")
+        self.make_test("\\")
+        self.make_test("//")
+        self.make_test("./,!@#$%^&*()!№;%:?*()")
+
+    def test_random_words(self):
+        for _ in range(100):
+            string_for_test = "".join(random.SystemRandom().choices(
+                string.ascii_letters + string.digits, k=random.randint(3, 25)))
+            self.make_test(string_for_test)
+
+if __name__ == "__main__":
+    unittest.main()
