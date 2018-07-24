a4792c05d21bc9c88223c18a37dd78923e125ef3
Dims <steelbreedman@gmail.com>
2018-07-24 18:16:12 +0500

    Написал README

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from steganography import Steganography
-from crypter import Crypter
-
 
 #ToDo добавить работу с аргументами
 #ToDo Проверить работу на всех файлах в архиве
-#ToDo Написать ReadMe
 #ToDo добавить параметр удалить старое сообщение
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     Steganography.encode_to_bmp('palm.bmp', "123")
     Steganography.decode_from_bmp('palm.bmp')
     Steganography.delete_message_from_bmp('palm.bmp', True)
