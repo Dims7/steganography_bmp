d2bad895028095bd3700ba3c92f6c49bc304a8f8
Dims <steelbreedman@gmail.com>
2018-07-24 20:20:42 +0500

    Исправил баги с аргументами

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys
 
 # ToDo добавить работу с аргументами
 # ToDo Проверить работу на всех файлах в архиве
-
 if __name__ == "__main__":
     flag_encode = False
     flag_decode = False
@@ -32,22 +31,25 @@ if __name__ == "__main__":
         elif arg == "-f":
             flag_path_file = True
 
-        flag_error = False
+    flag_error = False
 
-        if file_path is None:
-            print("You must enter the path to the file with the -f argument")
-            flag_error = True
-        if flag_encode and message is None:
-            print("You must enter a message with the -m argument")
-            flag_error = True
-        if not flag_error and flag_path_file:
-            if (flag_encode and flag_message and not flag_decode and
-                    not flag_delete):
-                Steganography.encode_to_bmp(file_path, message)
-            elif flag_decode and not flag_message and not flag_encode:
-                print(Steganography.decode_from_bmp(file_path))
-                if flag_delete:
-                    Steganography.delete_message_from_bmp(file_path, False)
-            elif (flag_delete and not flag_message and not flag_encode and
-                  not flag_message and not flag_decode):
-                Steganography.delete_message_from_bmp(file_path, True)
+    if file_path is None:
+        print("You must enter the path to the file with the -f argument")
+        flag_error = True
+    if flag_encode and message is None:
+        print("You must enter a message with the -m argument")
+        flag_error = True
+
+    if not flag_error and flag_path_file:
+        if (flag_encode and flag_message and not flag_decode and
+                not flag_delete):
+            Steganography.encode_to_bmp(file_path, message)
+        elif flag_decode and not flag_message and not flag_encode:
+            print(Steganography.decode_from_bmp(file_path))
+            if flag_delete:
+                Steganography.delete_message_from_bmp(file_path, False)
+        elif (flag_delete and not flag_message and not flag_encode and
+              not flag_message and not flag_decode):
+            Steganography.delete_message_from_bmp(file_path, True)
+        else:
+            print("Wrong arguments.")
